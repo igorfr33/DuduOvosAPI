b5eef114fcbbf8abf224aa213c2314a123f39899
Igor Rabelo <igorrabelof10@gmail.com>
2021-04-28 17:14:10 -0300

    Adcionado metodo Delete

--- src/main/java/br/com/duduovos/DuduOvosAPI/resorces/EntregasResults.java
+++ src/main/java/br/com/duduovos/DuduOvosAPI/resorces/EntregasResults.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,6 +42,12 @@ public class EntregasResults {
 	@PostMapping("/entrega")
 	public Entregas salvaEntrega(@RequestBody Entregas entrega) {
 		return entregasRepository.save(entrega);
+	}
+	
+	@ApiOperation(value = "Deleta uma entrega")
+	@DeleteMapping("/deletaentrega")
+	public void deletaEntrega(@RequestBody Entregas entrega) {
+	entregasRepository.delete(entrega);
 	}
 }
 		
